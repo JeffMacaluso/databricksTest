f5424d9ffbcbd918accc19af13bf4f0b424ce4d1
Jeff Macaluso <JeffMacaluso@users.noreply.github.com>
2018-02-05 14:03:17 -0600

    Initial commit

---
+++ notebooks/Users/jemacalu@microsoft.com/databricksTest.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.random.randn(100)
+y = np.random.randn(100)
+
+plt.scatter(X, y)
+
+# COMMAND ----------
+
