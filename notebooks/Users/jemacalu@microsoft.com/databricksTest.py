3ea9d24ec95041e05181304f96d6aa3bf6307830
Jeff Macaluso <JeffMacaluso@users.noreply.github.com>
2018-02-05 14:05:22 -0600

    Added plot

--- notebooks/Users/jemacalu@microsoft.com/databricksTest.py
+++ notebooks/Users/jemacalu@microsoft.com/databricksTest.py
@@ -1,11 +1,14 @@
 # Databricks notebook source
 import numpy as np
 import matplotlib.pyplot as plt
+%matplotlib inline
 
 X = np.random.randn(100)
 y = np.random.randn(100)
 
-plt.scatter(X, y)
+fig, ax = plt.subplots()
+ax.scatter(X, y)
+display(fig)
 
 # COMMAND ----------
 
